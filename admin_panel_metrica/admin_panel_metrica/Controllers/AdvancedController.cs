4ba79ad1013b839e81ecf1f904f6ca7c6c0390c6
elnurmehdi <elnurkm@code.edu.az>
2022-11-12 23:48:50 +0400

    AdvancedController and Layout created.

---
+++ admin_panel_metrica/admin_panel_metrica/Controllers/AdvancedController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace admin_panel_metrica.Controllers
+{
+    public class AdvancedController : Controller
+    {
+        public ActionResult Animation()
+        {
+            return View();
+        }
+
+    }
+}
