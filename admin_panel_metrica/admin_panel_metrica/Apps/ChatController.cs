33dd19450f9b0cf970c9b29ec4c04450c6fa8346
elnurmehdi <elnurkm@code.edu.az>
2022-11-13 19:04:42 +0400

    Apps controllers and actions added to source codes.

---
+++ admin_panel_metrica/admin_panel_metrica/Apps/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace admin_panel_metrica.Apps
+{
+    public class ChatController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
