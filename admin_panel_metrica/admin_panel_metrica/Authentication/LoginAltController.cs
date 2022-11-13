b786d83adba3c68af8f5e56e4af8e7dc493069ce
elnurmehdi <elnurkm@code.edu.az>
2022-11-13 19:27:53 +0400

    LoginAlt controller and action added to source codes.

---
+++ admin_panel_metrica/admin_panel_metrica/Authentication/LoginAltController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace admin_panel_metrica.Authentication
+{
+    public class LoginAltController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
