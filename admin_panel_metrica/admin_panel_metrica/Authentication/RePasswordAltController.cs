8a2410d7ecbfecf4779f9f03cf0e3728649f056c
elnurmehdi <elnurkm@code.edu.az>
2022-11-13 19:37:10 +0400

    RePassword and RePasswordAlt controllers and actions added to source codes.

---
+++ admin_panel_metrica/admin_panel_metrica/Authentication/RePasswordAltController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace admin_panel_metrica.Authentication
+{
+    public class RePasswordAltController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
