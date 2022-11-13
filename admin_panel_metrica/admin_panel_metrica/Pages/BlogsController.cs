b21d686f45488c099e4571e0669de16656f18884
elnurmehdi <elnurkm@code.edu.az>
2022-11-13 19:12:01 +0400

    Pages controllers and actions added to source codes.

---
+++ admin_panel_metrica/admin_panel_metrica/Pages/BlogsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace admin_panel_metrica.Pages
+{
+    public class BlogsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ admin_panel_metrica/admin_panel_metrica/Pages/StarterPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace admin_panel_metrica.Pages
+{
+    public class StarterPageController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
