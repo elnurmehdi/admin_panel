2b69197f33876ac4bae5d7b770cf256461d6ad08
elnurmehdi <elnurkm@code.edu.az>
2022-11-13 19:33:22 +0400

    Register and RegisterAlt controllers and actions added to source codes.

---
+++ admin_panel_metrica/admin_panel_metrica/Authentication/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace admin_panel_metrica.Authentication
+{
+    public class RegisterController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
